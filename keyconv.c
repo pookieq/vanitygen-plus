a674394f4e8cce034f73f8325c00cfc8d673ff1e
-k <slowdive@me.com>
2018-01-01 05:40:28 -0500

    Add AXE

--- keyconv.c
+++ keyconv.c
@@ -943,8 +943,8 @@ main(int argc, char **argv)
 			if (strcmp(optarg, "AXE")== 0) {
 				fprintf(stderr,
 					"Decrypting Axe Address\n");
-					addrtype_opt = 75;
-					privtype_opt = 203;
+					addrtype = 75;
+					privtype = 203;
 					break;
 			}	
 			else
